03d0edd4aeef75a573ffc0741106894008d9046d
Gabriel Frasson <bielfc27@gmail.com>
2023-04-05 10:18:14 -0300

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
+
 export function App() {
   return (
-    <div></div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      
+      <Transactions />
+    </ThemeProvider>
   )
 }
